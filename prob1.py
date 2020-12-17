82e68891feb55e8fd59c50897871f1c0e6767908
Eduardo Lima <eduardolimrib@gmail.com>
2020-12-17 01:02:30 -0300

    Fix comment character

--- prob1.py
+++ prob1.py
@@ -1,9 +1,9 @@
-'''
+"""
 Problema 1:
 Dado uma lista de dicionários (chave/valor) Python, verifique se existe a chave 'nome', 
 e caso exista, salve o valor dessa chave em uma segunda lista, de modo que não haja 
 repetição de valores na segunda lista.
-'''
+"""
 
 lista = [ 
     {
